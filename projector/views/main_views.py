e990d3f10f2a487fad30e82cb9d9af2ef0a98832
Ryang Sohn <loop.infinitely@gmail.com>
2021-07-02 15:26:42 +0900

    Format code

--- projector/views/main_views.py
+++ projector/views/main_views.py
@@ -1,7 +1,8 @@
-from flask import Blueprint, url_for
-from werkzeug.utils import redirect
+from flask import Blueprint, url_for, redirect
 
-bp = Blueprint('main', __name__, url_prefix='/')
-@bp.route('/')
+bp = Blueprint("main", __name__, url_prefix="/")
+
+
+@bp.route("/")
 def index():
-    return redirect(url_for('login.login'))
+    return redirect(url_for("login.login"))
