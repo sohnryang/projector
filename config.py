b748896ecc7a40abf659ba641b9f0686a73991ff
Ryang Sohn <loop.infinitely@gmail.com>
2021-07-02 23:17:06 +0900

    Add settings for JWT tokens

--- config.py
+++ config.py
@@ -5,4 +5,5 @@ SQLALCHEMY_DATABASE_URI = f'sqlite:///{os.path.join(BASE_DIR, "projector.db")}'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 GOOGLE_CLIENT_ID = os.environ.get("GOOGLE_CLIENT_ID", None)
 GOOGLE_CLIENT_SECRET = os.environ.get("GOOGLE_CLIENT_SECRET", None)
+JWT_SECRET_KEY = os.environ.get("JWT_SECRET_KEY", os.urandom(30).hex())
 SECRET_KEY = os.environ.get("SECRET_KEY", os.urandom(30).hex())
