7cbb39ed3d701936d64652a9a7e2ea7f92430a64
Ryang Sohn <loop.infinitely@gmail.com>
2021-07-06 12:43:51 +0900

    Add workaround for heroku postgresql

--- config.py
+++ config.py
@@ -4,6 +4,7 @@ BASE_DIR = os.path.dirname(__file__)
 SQLALCHEMY_DATABASE_URI = os.environ.get(
     "DATABASE_URL", "postgresql://curling_grad@localhost:5432/projector"
 )
+SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace("postgres:", "postgresql:")
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 GOOGLE_CLIENT_ID = os.environ.get("GOOGLE_CLIENT_ID", None)
 GOOGLE_CLIENT_SECRET = os.environ.get("GOOGLE_CLIENT_SECRET", None)
