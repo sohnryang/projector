afbdda8bc98f6f7de79abc5f75d431d98276fb8b
Ryang Sohn <loop.infinitely@gmail.com>
2021-07-08 15:14:11 +0900

    Add get in user api

--- projector/apis/user.py
+++ projector/apis/user.py
@@ -5,6 +5,15 @@ from projector.models import UserModel
 bp = Blueprint("user", __name__, url_prefix="/user")
 
 
+@bp.route("/get/<int:userid>", methods=["GET"])
+@login_required
+def get(userid: int):
+    user = UserModel.query.get(userid)
+    if user is None:
+        return Response(status=404)
+    return jsonify(user.serialize())
+
+
 @bp.route("/search-by-email/<email>", methods=["POST"])
 @login_required
 def search_by_email(email: str):
